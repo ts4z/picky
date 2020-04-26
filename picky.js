4514b5c14ac1696d4b0fd6919dac89591a4d5f5a
Tim Showalter <tjs@psaux.com>
2020-04-25 23:31:25 -0700

    Test data works, placeholder data added, safety feature implemented
    Can't accidentally clear winners now -- requires two clicks.
    
    Can sort by any of the 3 fields of a winner.

--- picky.js
+++ picky.js
@@ -1,9 +1,5 @@
 'use strict';
 
-function dlog(...args) {
-    // console.log(...args);
-}
-
 // rng picks a number between 0 and max.  This is a poor implementation and
 // probably has small biases.
 function rng(max) {
@@ -44,50 +40,62 @@ function splitLines(s) {
     var r = [];
     for (var i = 0; i < t.length; i++) {
         var tt = t[i].trim();
-        if (tt != "") {
+        if (tt !== "") {
             r.push(tt);
         }
     }
-    dlog("split output ", r, "length", r.length);
     return r;
 }
 
 // Expand multiplier labels.  ["3x A", "B"] => ["A","A","A","B"].  If someone's
 // name begins with /\d+x/, e.g., "3000x Bobby Tables", you can escape it with
-// 1x like this: "1x 3000x Bobby Tables".
+// 1x like this: "1x 3000x Bobby Tables".  And we actually don't care if the
+// 'x' is there anymore.
 function expandMultipliers(a) {
     var prizes = [];
-    var re = /(?:(\d+)x)\s+(.*)/;
+    var re = /(?:(\d+)x?)\s+(.*)/;
     for (var i = 0; i < a.length; i++) {
         var p = a[i];
         var match = re.exec(p);
         if (match) {
-            dlog("match", match[1], match[2]);
             for (var j = 0; j < match[1]; j++) {
                 prizes.push(match[2]);
             }
         } else {
-            dlog("noexpand ", p);
             prizes.push(p);
         }
     }
     return prizes;
 }
 
-function valueOrEmpty(el) {
+function valueOrDefault(el, def="") {
     var el = document.getElementById(el);
-    if (el != null) {
+    if (el !== null) {
         return el.value;
     }
-    dlog("no value in ", el);
-    return "";
+    return def;
 }
 
-function populateWinners() {
-    var id = "out";
-    var contestants = shuffle(expandMultipliers(splitLines(valueOrEmpty("contestants"))));
-    var prizes = expandMultipliers(splitLines(valueOrEmpty("prizes")));
-    var out = document.getElementById(id);
+var selectedWinners = undefined;
+const winnersId = "out";
+
+function actuallyClearWinners() {
+    document.getElementById(winnersId).value = "";
+    selectedWinners = undefined;
+    enable("selectWinners");
+    disable("clearWinners");
+}
+
+function maybeClearWinners() {
+    if (confirm("Are you sure you want to clear the winners list?")) {
+        actuallyClearWinners();
+    }
+}
+
+function selectWinners() {
+    var contestants = shuffle(expandMultipliers(splitLines(valueOrDefault("contestants"))));
+    var prizes = expandMultipliers(splitLines(valueOrDefault("prizes")));
+    var out = document.getElementById(winnersId);
 
     if (contestants.length < prizes.length) {
         out.value =
@@ -96,33 +104,77 @@ function populateWinners() {
         return;
     }
 
-    var v = "";
-    var l = (prizes.length + "").length;
+    var winners = [];
     for (var i = 0; i < prizes.length; i++) {
-        v += (i+1+"").padStart(l) +
-            " -- " + (contestants[i].padEnd(30)) +
-            " -- " + prizes[i] + "\n";
+        winners.push({natural: i, name: contestants[i], prize: prizes[i]});
     }
 
+    selectedWinners = winners;
+    populateWinnersBox();
+}
+
+function enable(id) {
+    document.getElementById(id).disabled = false;
+}
+
+function disable(id) {
+    document.getElementById(id).disabled = true;
+}
+
+function populateWinnersBox() {
+    if (selectedWinners.length == 0) {
+        return;
+    }
+    
+    enable("clearWinners");
+    disable("selectWinners");
+    
+    switch (valueOrDefault("sortBy")) {
+    case "name":
+        selectedWinners.sort((a,b) => a.name.localeCompare(b.name));
+        break;
+    case "prize":
+        selectedWinners.sort((a,b) => a.prize.localeCompare(b.prize));
+        break;
+    case "no": default:
+        selectedWinners.sort((a,b) => a.natural - b.natural);
+        break;
+    }
+    
+    var v = "";
+    var l = (selectedWinners.length + "").length;
+    for (var i = 0; i < selectedWinners.length; i++) {
+        var w = selectedWinners[i];
+        v += (i+1+"").padStart(l) +
+            " -- " + (w.name.padEnd(30)) +
+            " -- " + w.prize + "\n";
+    }
+
+    var out = document.getElementById(winnersId);
     out.value = v;
 }
 
 function relativeFile(f) {
-    // var here = location.href;
-    var here = "https://ts4z.net/picky/";
+    var here = location.href;
     return here.substring(0, here.lastIndexOf('/')) + '/' + f;
 }
 
 function fetchMy(file, andThen) {
     var req = new XMLHttpRequest();
     req.addEventListener("load", () => { andThen(req.responseText); })
+    req.addEventListener("error", () => {
+        console.alert("Couldn't load test data (failed). Just make some up.");
+    })
+    req.addEventListener("error", () => {
+        console.alert("Couldn't load test data (aborted). Just make some up.");
+    })
     req.open("GET", relativeFile(file));
     req.send();
 }
 
 function insertData(file, id) {
     var el = document.getElementById(id);
-    if (el == null) {
+    if (el === null) {
         console.log("can't find element id:", id);
         return;
     }
